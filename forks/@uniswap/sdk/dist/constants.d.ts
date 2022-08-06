cd56cc35faf68ba00d147d10cb2ec463b4452252
Terra-Credit <45095954+Terra-Credit@users.noreply.github.com>
2022-08-06 13:59:18 +0200

    1

--- forks/@uniswap/sdk/dist/constants.d.ts
+++ forks/@uniswap/sdk/dist/constants.d.ts
@@ -16,8 +16,8 @@ export declare enum Rounding {
     ROUND_HALF_UP = 1,
     ROUND_UP = 2
 }
-export declare const FACTORY_ADDRESS = "0x6Bd5A1A63ffF10De3c6B7C667040E9AE1B47fDf2";
-export declare const INIT_CODE_HASH = "0xaf88dd15a55596feb9d67243c727bfd6144af12453963809bc91f0cfcf8241bc";
+export declare const FACTORY_ADDRESS = "0x135ae61977dd57f975c5d9c69fecfeda902e8b97";
+export declare const INIT_CODE_HASH = "0xf0eddb5e286fe656e0610baa1ef689aa367e2386f49f22d99bcd600398b2c117";
 export declare const MINIMUM_LIQUIDITY: JSBI;
 export declare const ZERO: JSBI;
 export declare const ONE: JSBI;
